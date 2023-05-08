d093d0c21a9475984ad61ce8afde816d9167c0cc
Edward Kuan <edward880814@gmail.com>
2023-05-08 14:50:38 +0800

    feat: connect user and todos

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,12 +4,22 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos }) })
-    .catch((error) => console.error(error))
+//試著用async跟await的方式寫寫看
+router.get('/', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id)
+    if (!user) {
+      throw new Error('user not found')
+    }
+    const todos = await Todo.findAll({
+      raw: true,
+      nest: true,
+      where: { UserId: req.user.id }
+    })
+    res.render('index', { todos })
+  } catch (error) {
+    console.error(error)
+  }
 })
+
 module.exports = router
